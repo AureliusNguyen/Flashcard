1f3db9a63af3639d243862ba9a0435ed50e4b704
Rajesh Choudhary <rajeshchoudharyt@gmail.com>
2024-08-21 18:36:35 +0530

    Add route protection

--- app/api/flashcards/[id]/route.js
+++ app/api/flashcards/[id]/route.js
@@ -1,22 +1,11 @@
 import { supabase } from "@/utils/supabase";
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
 import { NextResponse } from "next/server";
 
-function checkAuthState() {
-	const { userId } = auth();
-	if (!userId) redirect("/sign-in");
-	return userId;
-}
-
 export async function GET(req, { params }) {
-	const user_id = checkAuthState();
-
 	const id = params.id;
 	const { data, error } = await supabase
 		.from("flashcards")
 		.select()
-		.eq("user_id", user_id)
 		.eq("id", id);
 
 	if (error) return NextResponse(error);
